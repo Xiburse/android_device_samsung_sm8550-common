73ff93316ba692155bd83a9999cb71889acabcfc
Xiburse <50038191+Xiburse@users.noreply.github.com>
2024-11-11 16:13:25 +0800

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -13,12 +13,6 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
 
-export TARGET_ENABLE_CHECKELF=true
-
-# If XML files don't have comments before the XML header, use this flag
-# Can still be used with broken XML files by using blob_fixup
-export TARGET_DISABLE_XML_FIXING=true
-
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
@@ -34,6 +28,7 @@ ONLY_FIRMWARE=
 ONLY_TARGET=
 KANG=
 SECTION=
+CARRIER_SKIP_FILES=()
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
@@ -70,50 +65,13 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        vendor/bin/hw/android.hardware.security.keymint-service|vendor/lib64/libskeymint10device.so|vendor/lib64/libskeymint_cli.so)
+        vendor/lib/libsample5.so)
             [ "$2" = "" ] && return 0
-            grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || ${PATCHELF} --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
-            ${PATCHELF} --replace-needed libcrypto.so libcrypto-v33.so "${2}"
-            ${PATCHELF} --replace-needed libcppbor_external.so libcppbor.so "${2}"
+            "${PATCHELF}" --replace-needed "libsample6.so" "libsample7.so" "${2}"
             ;;
-        vendor/lib64/hw/gatekeeper.mdfpp.so)
+        vendor/lib/libsample7.so)
             [ "$2" = "" ] && return 0
-            ${PATCHELF} --replace-needed libcrypto.so libcrypto-v33.so "${2}"
-            ;;
-        vendor/lib64/libsec-ril.so)
-            [ "$2" = "" ] && return 0
-            xxd -p -c0 "${2}" | sed "s/600e40f9e10315aa820c8052e30314aa/600e40f9e10315aa820c8052030080d2/g" | xxd -r -p > "${2}".patched
-            mv "${2}".patched "${2}"
-            sed -i 's/ril.dds.call.ongoing/vendor.calls.slot_id/g' "${2}"
-            ;;
-        vendor/etc/vintf/manifest/sec_c2_manifest_default0_1_0.xml)
-            [ "$2" = "" ] && return 0
-            sed -i 's/default0/software/g' "${2}"
-            ;;
-        vendor/etc/media_codecs_kalama.xml|vendor/etc/media_codecs_kalama_vendor.xml)
-            [ "$2" = "" ] && return 0
-            sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio)/d" "${2}"
-            ;;
-        vendor/etc/seccomp_policy/atfwd@2.0.policy)
-            [ "$2" = "" ] && return 0
-            grep -q "gettid: 1" "${2}" || echo -e "\ngettid: 1" >> "${2}"
-            ;;
-        vendor/etc/seccomp_policy/qwesd@2.0.policy)
-            [ "$2" = "" ] && return 0
-            grep -q "gettid: 1" "${2}" || echo -e "\ngettid: 1" >> "${2}"
-            grep -q "pipe2: 1" "${2}" || echo -e "\npipe2: 1" >> "${2}"
-            ;;
-        vendor/lib64/libqcodec2_core.so)
-            [ "$2" = "" ] && return 0
-            grep -q "libcodec2_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcodec2_shim.so" "${2}"
-            ;;
-        vendor/etc/init/vendor.qti.media.c2audio@1.0-service.rc)
-            [ "$2" = "" ] && return 0
-            sed -i '/disabled/d' "${2}"
-            ;;
-        vendor/lib64/unihal_android.so)
-            [ "$2" = "" ] && return 0
-            grep -q libui_shim.so "$2" || "$PATCHELF" --add-needed libui_shim.so "$2"
+            "${PATCHELF}" --set-soname "libsample7.so" "${2}"
             ;;
         *)
             return 1
@@ -125,6 +83,18 @@ function blob_fixup() {
 
 function blob_fixup_dry() {
     blob_fixup "$1" ""
+}
+
+function prepare_firmware() {
+    if [ "${SRC}" != "adb" ]; then
+        local STAR="${ANDROID_ROOT}"/lineage/scripts/motorola/star.sh
+        for IMAGE in bootloader radio; do
+            if [ -f "${SRC}/${IMAGE}.img" ]; then
+                echo "Extracting Motorola star image ${SRC}/${IMAGE}.img"
+                sh "${STAR}" "${SRC}/${IMAGE}.img" "${SRC}"
+            fi
+        done
+    fi
 }
 
 if [ -z "${ONLY_FIRMWARE}" ] && [ -z "${ONLY_TARGET}" ]; then
@@ -141,6 +111,13 @@ if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/propriet
 
     if [ -z "${ONLY_FIRMWARE}" ]; then
         extract "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+        if [ -f "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files-carriersettings.txt" ]; then
+            generate_prop_list_from_image "product.img" "${MY_DIR}/../../proprietary-files-carriersettings.txt" CARRIER_SKIP_FILES carriersettings
+            extract "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files-carriersettings.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+            extract_carriersettings
+        fi
     fi
 
     if [ -z "${SECTION}" ] && [ -f "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt" ]; then
